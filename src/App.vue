ab36355c4184ecca5d64095b8842f63aae114857
CharmineX <boedhramkenneth@gmail.com>
2025-04-05 01:11:27 +0200

    updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue';
+import Hero from './components/Hero.vue';
+import Skills from './components/Skills.vue';
+import Services from './components/Services.vue';
+import Projects from './components/Projects.vue';
+import Testimonials from './components/Testimonials.vue';
+import Contact from './components/Contact.vue';
+import Footer from './components/Footer.vue';
+
+import { onMounted } from "vue";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+onMounted(() => {
+  AOS.init({
+    duration: 1800,
+    offset: 100,
+    disable: "mobile",
+  });
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<div class="bg-gray-950">
+      <Navbar />
+      <Hero />
+      <Skills />
+      <Services />
+      <Projects />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
